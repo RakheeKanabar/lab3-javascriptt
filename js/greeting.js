d5be8c02d7962f71f8746a64793e40d262389830
Rakhee Kanabar <P15203891@LEC-ADMIN.dmu.ac.uk>
2017-10-19 10:51:03 +0100

    i dont know what i done

--- js/greeting.js
+++ js/greeting.js
@@ -2,9 +2,12 @@
   let myName = document.getElementById("my-name"); 
   let userName = document.getElementById("user-name"); 
   let getName = document.getElementById("get-name");
+  let getHeader = document.getElementById("myheader");
+  let getuser-name = document.getElementById("user-name");
   
   function PerformGreeting() {
     myName.innerHTML = userName.value;
+	getHeader.innerHTML = userName.value + "" + surname.value;
     event.preventDefault();
     return false;
   }
